7fad0f0a0c82189a6e157c1561a7377cda40aea5
caemci <carlosecimino@gmail.com>
2019-03-02 19:48:30 -0300

    Segunda version corregida

--- src/figuras/Principal.java
+++ src/figuras/Principal.java
@@ -1,24 +1,34 @@
 package figuras;
 
-/**
- *
- * @author caemci
- */
 public class Principal {
 
     public static void main(String[] args) {
-        // Creo algunos objetos
-        Rectangulo r1 = new Rectangulo("Bandera", 2, 3);
-        TrianguloEquilatero tt1 = new TrianguloEquilatero("Triangulin", 4);
-        Circunferencia c1 = new Circunferencia("Anillo", 5);
-        TrianguloEquilatero tt2 = new TrianguloEquilatero("Flecha", 3);
-        Circunferencia c2 = new Circunferencia("Moneda", 2);
-
-        // Para mostrar todas las figuras, debo invocar los métodos, objeto por objeto
+        
+        // Creo algunos objetos con distintas combinaciones
+        
+        // VALIDO
+        Rectangulo r1 = new Rectangulo("Bandera1", 2, 3);
         r1.dibujar();
-        tt1.dibujar();
-        c1.dibujar();
-        tt2.dibujar();
-        c2.dibujar();
+        System.out.println("Area: " + r1.area() );
+        
+        System.out.println("");
+        
+        // VALIDO
+        FiguraGeometrica r2 = new FiguraGeometrica("FiguraGenerica"); // Un Rectangulo 'cabe' en una FiguraGeometrica.
+        r2.dibujar();
+        // System.out.println("Area: " + r2.area() ); No se puede. FiguraGeometrica no tiene un método area()
+        
+        System.out.println("");
+        
+        // VALIDO
+        FiguraGeometrica r3 = new Rectangulo("Bandera2", 2, 3); // Un Rectangulo 'cabe' en una FiguraGeometrica.
+        // r3 se comportará como una FiguraGeometrica genérica
+        r3.dibujar(); // Se invoca el de la superclase
+        // System.out.println("Area: " + r3.area() ); No se puede. FiguraGeometrica no tiene un método area()
+        
+        System.out.println("");
+        
+        // NO VALIDO
+        // Rectangulo r4 = new FiguraGeometrica("FiguraGenerica"); // No se puede. Una FiguraGeometrica no 'cabe' en un Rectangulo.
     }
 }
